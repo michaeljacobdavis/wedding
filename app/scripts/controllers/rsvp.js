fbb124e0a7d7a6fc3913437a4a226cfb5b39eabf
MJD <mike@michaeljacobdavis.com>
2013-02-16 21:14:14 -0800

    Added mongodb backend

--- app/scripts/controllers/rsvp.js
+++ app/scripts/controllers/rsvp.js
@@ -4,7 +4,7 @@
 
 weddingApp.controller('RsvpCtrl', function($scope, $http) {
 
-	var url = 'https://docs.google.com/spreadsheet/formResponse?formkey=dFRORWZQSHh5aDJiM3dLUU9ldmlQNmc6MQ&theme=0AX42CRMsmRFbUy0yMjdiMTQ4Yi0zZjUwLTQ5NTUtOGVmNC05ODNlZTUxYTViYzA&embedded=true&ifq';
+	var url = '/api/rsvp';
 	$scope.save = function() {
 	
 		$http.post(url, $scope.entry);
---
+++ models/rsvp.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema;
+
+var rsvpSchema = new Schema({
+	name: String,
+	coming: Boolean,
+	howMany: Number
+});
+
+exports.Rsvp = mongoose.model('Rsvp', rsvpSchema);
