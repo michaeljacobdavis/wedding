e450a8a99349d985f383e028933e40c99b626cc8
MJD <mike@michaeljacobdavis.com>
2013-02-16 18:35:16 -0800

    added mongo db env

--- app/scripts/controllers/rsvp.js
+++ app/scripts/controllers/rsvp.js
@@ -4,11 +4,9 @@
 
 weddingApp.controller('RsvpCtrl', function($scope, $http) {
 
-	// Todo:
-	// This is already on the form
-	// it should probably be grabbed from there
 	var url = 'https://docs.google.com/spreadsheet/formResponse?formkey=dFRORWZQSHh5aDJiM3dLUU9ldmlQNmc6MQ&theme=0AX42CRMsmRFbUy0yMjdiMTQ4Yi0zZjUwLTQ5NTUtOGVmNC05ODNlZTUxYTViYzA&embedded=true&ifq';
 	$scope.save = function() {
+	
 		$http.post(url, $scope.entry);
 	};
 });
