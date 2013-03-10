5e2a272e879b5b01a3ae038d6ba3524ad18b23ec
Michael Davis <michaeldavis@unknown20c9d07958a7.att.net>
2013-03-09 19:50:24 -0800

    Added directions

--- app/scripts/controllers/directions.js
+++ app/scripts/controllers/directions.js
@@ -3,5 +3,8 @@
 'use strict';
 
 weddingApp.controller('DirectionsCtrl', function($scope) {
-
+  var map = L.map('map').setView([33.238251,-117.236256], 10);
+  L.tileLayer('http://{s}.tile.cloudmade.com/efbcd8f7d1d746819abd2aee833d814b/997/256/{z}/{x}/{y}.png').addTo(map);
+  var marker = L.marker([33.238251,-117.236256]).addTo(map);
+  marker.bindPopup('676 Osborne St, Vista, CA 92084');
 });
